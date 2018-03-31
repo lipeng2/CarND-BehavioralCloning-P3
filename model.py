f06efd6240c38b99807cff789ee26376feae3d00
michael peng <lipeng2@illinois.edu>
2018-03-30 20:25:09 -0400

    update model and add sample images of training data

--- model.py
+++ model.py
@@ -1,16 +1,27 @@
 import retrive_data as rd
-import process_data as pro
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout
 from keras import optimizers
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 # directory of training dataset
 dir = 'train/'
+samples = rd.get_lines(dir)
 
-# obtain preprocess training data
-x,y = rd.get_data(dir, addition=True, adjustment=0.2)
-x_aug, y_aug = pro.data_augment(x,y)
+# obtain training and validation sets
+train_samples, val_samples = train_test_split(samples, test_size=0.2)
+
+# parameters
+keep_prob = 0.3 # keep_prob for dropout layer
+epochs = 5 # epochs for training
+batch_size = 12 # batch_size
+learning_rate = 0.0001 # learning rate of adam optimizer
+
+# create data generators
+train_generator = rd.generator(train_samples, batch_size, addition=False)
+val_generator = rd.generator(val_samples, batch_size)
 
 # model
 model = Sequential()
@@ -31,18 +42,21 @@ model.add(Conv2D(64,3,3,activation='relu'))
 # fully connected layer
 model.add(Flatten())
 # fc with 100 ouput units
-model.add(Dense(100))
+model.add(Dense(100, activation='relu'))
 # fc with 50 output units
-model.add(Dense(50))
+model.add(Dense(50, activation='relu'))
+# dropout layer with keep_prob
+model.add(Dropout(keep_prob))
 # fc with 10 output units
-model.add(Dense(10))
+model.add(Dense(10, activation='relu'))
 # fc with 1 output unit
 model.add(Dense(1))
 
 # compile model using adam optimizer with learning rate of 0.0001 wiht no decay
-adam = optimizers.Adam(lr=0.0001)
+adam = optimizers.Adam(lr=learning_rate, decay=1e-6)
 model.compile(loss='mean_squared_error', optimizer=adam)
-model.fit(x_aug, y_aug, epochs=10, validation_split=0.2, shuffle=True)
+
+model.fit_generator(train_generator, steps_per_epoch= len(train_samples)//batch_size, validation_data=val_generator, validation_steps=len(val_samples)//batch_size, epochs=5, shuffle=True, workers=4)
 
 # save model
-model.save('advance_model.h5')
+model.save('model.h5')
--- retrive_data.py
+++ retrive_data.py
@@ -1,8 +1,16 @@
 import csv
 import cv2
 import numpy as np
+from sklearn.utils import shuffle
 
 def get_lines(dir):
+    '''
+    Input:
+    dir -- name of the dictory where the dataset is stored
+
+    Return:
+    lines -- list of filenames of images and values of steering angles
+    '''
     lines = []
     with open(f'{dir}driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
@@ -10,27 +18,40 @@ def get_lines(dir):
             lines.append(line)
     return lines
 
-def get_data(dir, addition=False, adjustment=0.2):
-    images = []
-    measurements = []
-    for line in get_lines(dir):
-        file = line[0].split('\\')[-1]
-        img = cv2.imread(f'{dir}IMG/{file}')
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        images.append(img)
-        measurements.append(float(line[3]))
-        if addition:
-            for i in range(1,3):
-                file = line[i].split('\\')[-1]
-                img = cv2.imread(f'{dir}IMG/{file}')
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                images.append(img)
-                measurement = float(line[3])
-                if i == 1: measurement += adjustment
-                if i == 2: measurement -= adjustment
-                measurements.append(measurement)
+# helper function
+def generate_data(batch_samples, addition=False, adjustment=0.2):
+    images, angles = [], []
+    for batch_sample in batch_samples:
+        num_of_cameras = 3 if addition else 1
+        for i in range(num_of_cameras):
+            filename = batch_sample[i]
+            img = cv2.imread(filename)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            angle = float(batch_sample[3])
+            if i ==1: angle += adjustment
+            if i ==2: angle -= adjustment
+            images.append(img)
+            images.append(cv2.flip(img,1))
+            angles.append(angle)
+            angles.append(-1.0*angle)
+    return np.array(images), np.array(angles)
 
-    X_train = np.array(images)
-    y_train = np.array(measurements)
+# data generator
+def generator(samples, batch_size=32, addition=False, adjustment=0.2):
+    '''
+    Input:
+    samples -- Dataset from which data generated
+    batch_size -- size of a single batch of samples
+    addition -- boolean value. Default value is False. When equal true, generators use data obtained from all three cameras
+    adjustment -- The value add or substracted from angles when using data obtained from all three cameras
 
-    return X_train, y_train
+    Return:
+    Images -- list of images data
+    angles -- list of steering angles
+    '''
+    n = len(samples)
+    while True:
+        samples = shuffle(samples)
+        for offset in range(0, n, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            yield generate_data(batch_samples, addition, adjustment)
